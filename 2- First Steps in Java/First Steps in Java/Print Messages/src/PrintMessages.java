df19f068a833dcb28faf5e03ee676fcd699c6172
yourname <youraddress@company.com>
2024-11-16 18:35:22 +0800

    2

---
+++ 2- First Steps in Java/First Steps in Java/Print Messages/src/PrintMessages.java
@@ -0,0 +1,9 @@
+public class PrintMessages
+{
+   public static void main(String[] args)
+   {
+
+      System.out.print("This is Java Programming Basics.\n" +
+              "I can print messages in Java!");
+   }
+}
